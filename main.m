52d78eff46e289055913662c6ccc6739f64f6aa8
Dionne Ariens <dlr.ariens@outlook.com>
2021-03-29 13:59:19 +0200

    adding images and basic .m file

--- main.m
+++ main.m
@@ -1,3 +1,7 @@
-for i = 1:17
-    sin(i)
-end
+%% Project 2: Man on Board
+%% Course: Image processing and computer vision
+%% Group: 1
+%% Authors: Dionne Ariens, Jesse van Werven, Roxane Munsterman
+%% Date: 29-03-2021
+
+clear all; close all; clc;
