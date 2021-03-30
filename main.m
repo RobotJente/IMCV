53087cdb79078cdb0ecdebe906dfb6d50b3800df
Roxane Munsterman <roxanemunsterman@gmail.com>
2021-03-30 15:44:01 +0200

    calibration

--- main.m
+++ main.m
@@ -5,3 +5,12 @@
 %% Date: 29-03-2021
 
 clear all; close all; clc;
+v = VideoReader('MAH01462.MP4')
+
+%% Camera calibration by checkerboard calibration images
+%Load calibration images from zip file
+cal_images=imageSet('C:\Users\Roxan\Desktop\IPCV_Project\calibration_images');
+
+[cameraParams]=CameraCalibration(cal_images)
+   
+   
